135c911db518c6e4a8762e4a6d1edf96ab8925ad
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-07-18 02:03:15 -0300

    Projeto final

---
+++ README.md
@@ -0,0 +1,1 @@
+# java-oo-banco-digital
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente();
+		cliente.setNome("matheus");
+		
+		Conta cc = new ContaCorrente(cliente);
+		cc.depositar(100);
+		
+		Conta poupanca = new ContaPoupanca(cliente);
+		
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		
+		poupanca.imprimirExtrato();
+	}
+
+}
